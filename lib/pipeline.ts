1975fef02f60ead11bbab79861b485c534de65c1
Rodrigo Strazza <rstrazza@gmail.com>
2022-05-20 03:18:33 +0000

    Remove test and prod stacks

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -67,18 +67,18 @@ export default class PipelineConstruct extends Construct {
             id: "dev",
             stackBuilder: blueprint.clone("us-west-2").addOns(devBootstrapArgo),
           },
-          {
-            id: "test",
-            stackBuilder: blueprint
-              .clone("us-east-2")
-              .addOns(testBootstrapArgo),
-          },
-          {
-            id: "prod",
-            stackBuilder: blueprint
-              .clone("us-east-1")
-              .addOns(prodBootstrapArgo),
-          },
+          //   {
+          //     id: "test",
+          //     stackBuilder: blueprint
+          //       .clone("us-east-2")
+          //       .addOns(testBootstrapArgo),
+          //   },
+          //   {
+          //     id: "prod",
+          //     stackBuilder: blueprint
+          //       .clone("us-east-1")
+          //       .addOns(prodBootstrapArgo),
+          //   },
         ],
       })
       .build(scope, id + "-stack", props);
